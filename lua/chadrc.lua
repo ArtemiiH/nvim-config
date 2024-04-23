a9ac5fb34afb37a4df1b1907199b5bf3313cdab4
Artemii Hrynevych <artemiihrynevych@gmail.com>
2024-04-23 11:15:55 +0200

    Some styling changes to chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,7 +2,6 @@
 local M = {}
 
 M.ui = {
-  transparency = true,
   theme = "catppuccin",
   theme_toggle = { "catppuccin", "gruvbox_light" },
 
@@ -18,6 +17,25 @@ M.ui = {
 
   nvdash = {
     load_on_startup = true,
+  },
+
+  telescope = {
+    style = "bordered",
+  },
+
+  statusline = {
+    theme = "vscode_colored",
+  },
+
+  term = {
+    float = {
+      relative = "editor",
+      row = 0.25,
+      col = 0.2,
+      width = 0.6,
+      height = 0.5,
+      border = "single",
+    },
   },
 }
 
