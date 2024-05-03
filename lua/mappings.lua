98e323d416fceb7ae78699d54c7f7f777d0084f4
Artemii Hrynevych <artemiihrynevych@gmail.com>
2024-05-03 22:47:20 +0200

    Remove mapping left after auto-session plugin

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -61,6 +61,3 @@ map("n", "<leader>gsy", "<cmd>GoTagAdd taml <CR>", { desc = "Gopher Add yaml str
 map("n", "[c", function()
   require("treesitter-context").go_to_context()
 end, { desc = "treesitter-context Jump to context (upwards)" })
-
--- Sessions
-map("n", "<leader>ps", "<cmd>SearchSession<CR>", { desc = "Telescope Find sessions" })
