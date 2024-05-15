d3cd0baba1e902408406bc52ea44cbde4923616c
Artemii Hrynevych <artemii.hrynevych@ibm.com>
2024-05-15 14:32:23 +0200

    Remove automatic debug UI openning, add prettier to mason

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,6 +29,7 @@ return {
         "css-lsp",
         "html-lsp",
         "typescript-language-server",
+        "prettier",
 
         -- shell stuff
         "shfmt",
@@ -79,15 +80,6 @@ return {
       local dap = require "dap"
       local dapui = require "dapui"
       dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.after.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.after.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
     end,
   },
 
