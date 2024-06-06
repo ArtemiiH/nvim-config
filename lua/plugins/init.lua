88bb5aef22545876cbaa025c1b0554ea5a5d6815
Artemii Hrynevych <artemii.hrynevych@ibm.com>
2024-06-06 22:09:14 +0200

    Add code outline plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,4 +127,15 @@ return {
       vim.fn["mkdp#util#install"]()
     end,
   },
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      -- Your setup opts here
+    },
+  },
 }
