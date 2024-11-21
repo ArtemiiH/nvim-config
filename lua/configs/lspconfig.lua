7ac72617a3787f2a73ea1f74cbae2d1b2a5ac326
Artemii Hrynevych <artemii.hrynevych@ibm.com>
2024-11-21 15:45:56 +0100

    Add nil_ls LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -66,7 +66,7 @@ lspconfig.lua_ls.setup {
   },
 }
 
-local servers = { "html", "cssls", "ts_ls", "pyright", "clangd", "marksman" }
+local servers = { "html", "cssls", "ts_ls", "pyright", "clangd", "marksman", "nil_ls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
