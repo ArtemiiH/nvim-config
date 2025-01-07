857d109ab2a47de88404bce19a84029edd5122de
Artemii Hrynevych <artemii.hrynevych@ibm.com>
2025-01-07 10:50:03 +0100

    Increase on yank highlighting timeout

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlighting when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank { timeout = 300 }
   end,
 })
 
