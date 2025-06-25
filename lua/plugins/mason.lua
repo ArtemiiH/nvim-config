8898f826cbb7b8c54c249841d510d5a4b9a6c79d
Artemii Hrynevych <artemiihrynevych@gmail.com>
2025-06-25 16:06:17 +0200

    Remove mason

--- lua/plugins/mason.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "williamboman/mason.nvim",
-  cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-  dependencies = { "williamboman/mason-lspconfig.nvim" },
-  lazy = false,
-  config = function(_, opts)
-    require("mason").setup(opts)
-  end,
-}
