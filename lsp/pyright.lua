4882cc4857e78f4d4808c8e3ecc4d61c998ba2ee
Artemii Hrynevych <artemiihrynevych@gmail.com>
2025-06-25 16:01:06 +0200

    Remove utils, they are unused

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -1,7 +1,6 @@
 return {
   cmd = { "pyright-langserver", "--stdio" },
   filetypes = { "python" },
-  -- Using a utility to find the project root is still very common and useful
   root_markers = { "pyproject.toml", "setup.py", ".git" },
   settings = {
     python = {
